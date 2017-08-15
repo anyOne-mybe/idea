9ead0ceb3a47c5f8bdf6f44ca69ffc74e6d0986a
guanhaobi <guanhaobi@163.com>
2017-08-15 21:39:25 +0800

    update web files

--- idea-web/src/main/webapp/idea/js/IdeaInit.js
+++ idea-web/src/main/webapp/idea/js/IdeaInit.js
@@ -3,7 +3,6 @@
 	initIdeaContext();
 	
 	function initIdeaContext(){
-		debugger;
 		var hash = (location.hash || '').slice(2);
 		if(hash){
 			Idea.Page.forward(hash);
@@ -42,7 +41,11 @@
 	};
 	
 	function addMenue(node,$menuedDoms){
-		var $node = $('<div class="nav_item">'+node.name+'</div>');
+		var nodeDom = [];
+		nodeDom.push('<div class="nav_item"><span class="nav_name">'+node.name);
+		nodeDom.push('</span></div>');
+		
+		var $node = $(nodeDom.join(''));
 		$node.data('value',node).appendTo($menuedDoms);
 	};
 	
@@ -50,7 +53,49 @@
 		$menuedDoms.on('click','.nav_item',function(e){
 			var $this = $(this);
 			Idea.Page.forward($this.data('value').url);
-		})
+		});
+		
+		$menuedDoms.on('mouseenter','.nav_item',function(e){
+			debugger;
+			var $this = $(this);
+			var data = $this.data('value');
+			//展示子菜单
+			_showSunMenues($this,data);
+			
+//			Idea.Page.forward($this.data('value').url);
+		});
+		
+		$menuedDoms.on('mouseleave','.nav_item',function(e){
+			debugger;
+			var $this = $(this);
+			var data = $this.data('value');
+			//展示子菜单
+			_hideSunMenues($this,data);
+			
+//			Idea.Page.forward($this.data('value').url);
+		});
+	};
+	
+	function _showSunMenues($menue,node){
+		if($menue.find('.sub_menue_container').length <= 0){
+			var subDom = [],
+			children = node.children || [],
+			subNode = null;
+			subDom.push('<div class="sub_menue_container">');
+			for(var i = 0; i < children.length; i++){
+				subNode = children[i];
+				subDom.push('<div class="sub_item">'+subNode.name+'</div>');
+			}
+			subDom.push('</div>');
+			
+			$menue.append(subDom.join(''));
+		}else{
+			$menue.find('.sub_menue_container').removeClass('idra_hide');
+		}
+	};
+	
+	function _hideSunMenues($menue,node){
+		$menue.find('.sub_menue_container').addClass('idra_hide');
 	};
 	
 	
